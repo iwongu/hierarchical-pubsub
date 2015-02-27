82dbc18a9bffd3df189fdd24b3c5979e154461e9
Wongoo Lee <iwongu@gmail.com>
2015-02-27 07:57:45 -0800

    Clean up examples.

--- examples/index.js
+++ examples/index.js
@@ -1,51 +1,22 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var pubsub = require('..');
 
-var pubsub = require('../lib/pubsub');
-require('./light');
-require('./display');
-require('./controller');
+pubsub('foo/bar').
+  on('value', function(data) {
+    console.log(data.event); // 'foo/bar/zoo'
+    console.log(data.value); // 'goo'
+  });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+pubsub('other/node/1/2/3').
+  on('value', function(data) {
+    console.log(data.event); // 'other/node/1/2/3'
+    console.log(data.value); // 'goo'
+  });
 
-var clients = {};
-
-var living_room_motion_1 = pubsub('motion/living_room/1');
-var living_room_light = pubsub('light/living_room');
-var living_room_display = pubsub('display/living_room');
-
-io.on('connection', function (socket) {
-  clients[socket.id] = socket;
-  socket.
-    on('disconnect', function() {
-      delete clients[socket.id];
-    }).
-    on('motion', function(msg) {
-      living_room_motion_1.setValue(msg);
+pubsub('foo/bar').
+  on('value', function(data) {
+    pubsub('other/node').visitDescendants(function(node) {
+      node.setValue('goo');
     });
-});
-
-var broadcast = function(event, msg) {
-  for (var client in clients) {
-    clients[client].emit(event, msg);
-  }
-};
-
-living_room_light.
-  on('value', function(data) {
-    broadcast('light', data.event + ': ' + data.value.status);
   });
 
-living_room_display.
-  on('value', function(data) {
-    broadcast('display', data.event + ': ' + data.value.message);
-  });
-
-server.listen(3000, function() {
-  console.log('listening on *:3000');
-});
-
+pubsub('foo/bar/zoo').setValue('goo');
